e1b2707dd9d2f66bc31fd3ea2bd504a67abb700b
shaks94 <shubham4516@gmail.com>
2019-02-22 04:39:55 +0530

    digit-recognizer-live

---
+++ js/app.js
@@ -0,0 +1,325 @@
+//-------------------
+// GLOBAL variables
+//-------------------
+var modelName = "digitrecognizermlp";
+let model;
+
+var canvasWidth           	= 150;
+var canvasHeight 			= 150;
+var canvasStrokeStyle		= "white";
+var canvasLineJoin			= "round";
+var canvasLineWidth       	= 10;
+var canvasBackgroundColor 	= "black";
+var canvasId              	= "canvas";
+
+var clickX = new Array();
+var clickY = new Array();
+var clickD = new Array();
+var drawing;
+
+document.getElementById('chart_box').innerHTML = "";
+document.getElementById('chart_box').style.display = "none";
+
+//---------------
+// Create canvas
+//---------------
+var canvasBox = document.getElementById('canvas_box');
+var canvas    = document.createElement("canvas");
+
+canvas.setAttribute("width", canvasWidth);
+canvas.setAttribute("height", canvasHeight);
+canvas.setAttribute("id", canvasId);
+canvas.style.backgroundColor = canvasBackgroundColor;
+canvasBox.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+  canvas = G_vmlCanvasManager.initElement(canvas);
+}
+
+ctx = canvas.getContext("2d");
+
+//-----------------------
+// select model handler
+//-----------------------
+$("#select_model").change(function() {
+  	var select_model  = document.getElementById("select_model");
+  	var select_option = select_model.options[select_model.selectedIndex].value;
+
+  	if (select_option == "MLP") {
+  		modelName = "digitrecognizermlp";
+
+  	} else if (select_option == "CNN") {
+  		modelName = "digitrecognizercnn";
+
+  	} else {
+  		modelName = "digitrecognizermlp";
+  	}
+
+  	loadModel(modelName);
+});
+
+//---------------------
+// MOUSE DOWN function
+//---------------------
+$("#canvas").mousedown(function(e) {
+	var mouseX = e.pageX - this.offsetLeft;
+	var mouseY = e.pageY - this.offsetTop;
+
+	drawing = true;
+	addUserGesture(mouseX, mouseY);
+	drawOnCanvas();
+});
+
+//-----------------------
+// TOUCH START function
+//-----------------------
+canvas.addEventListener("touchstart", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+
+	var rect = canvas.getBoundingClientRect();
+	var touch = e.touches[0];
+
+	var mouseX = touch.clientX - rect.left;
+	var mouseY = touch.clientY - rect.top;
+
+	drawing = true;
+	addUserGesture(mouseX, mouseY);
+	drawOnCanvas();
+
+}, false);
+
+//---------------------
+// MOUSE MOVE function
+//---------------------
+$("#canvas").mousemove(function(e) {
+	if(drawing) {
+		var mouseX = e.pageX - this.offsetLeft;
+		var mouseY = e.pageY - this.offsetTop;
+		addUserGesture(mouseX, mouseY, true);
+		drawOnCanvas();
+	}
+});
+
+//---------------------
+// TOUCH MOVE function
+//---------------------
+canvas.addEventListener("touchmove", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	if(drawing) {
+		var rect = canvas.getBoundingClientRect();
+		var touch = e.touches[0];
+
+		var mouseX = touch.clientX - rect.left;
+		var mouseY = touch.clientY - rect.top;
+
+		addUserGesture(mouseX, mouseY, true);
+		drawOnCanvas();
+	}
+}, false);
+
+//-------------------
+// MOUSE UP function
+//-------------------
+$("#canvas").mouseup(function(e) {
+	drawing = false;
+});
+
+//---------------------
+// TOUCH END function
+//---------------------
+canvas.addEventListener("touchend", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	drawing = false;
+}, false);
+
+//----------------------
+// MOUSE LEAVE function
+//----------------------
+$("#canvas").mouseleave(function(e) {
+	drawing = false;
+});
+
+//-----------------------
+// TOUCH LEAVE function
+//-----------------------
+canvas.addEventListener("touchleave", function (e) {
+	if (e.target == canvas) {
+    	e.preventDefault();
+  	}
+	drawing = false;
+}, false);
+
+//--------------------
+// ADD CLICK function
+//--------------------
+function addUserGesture(x, y, dragging) {
+	clickX.push(x);
+	clickY.push(y);
+	clickD.push(dragging);
+}
+
+//-------------------
+// RE DRAW function
+//-------------------
+function drawOnCanvas() {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	ctx.strokeStyle = canvasStrokeStyle;
+	ctx.lineJoin    = canvasLineJoin;
+	ctx.lineWidth   = canvasLineWidth;
+
+	for (var i = 0; i < clickX.length; i++) {
+		ctx.beginPath();
+		if(clickD[i] && i) {
+			ctx.moveTo(clickX[i-1], clickY[i-1]);
+		} else {
+			ctx.moveTo(clickX[i]-1, clickY[i]);
+		}
+		ctx.lineTo(clickX[i], clickY[i]);
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
+
+//------------------------
+// CLEAR CANVAS function
+//------------------------
+function clearCanvas(id) {
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	clickX = new Array();
+	clickY = new Array();
+	clickD = new Array();
+}
+
+//-------------------------------------
+// loader for digitrecognizermlp model
+//-------------------------------------
+async function loadModel(modelName) {
+  console.log("model loading..");
+
+  // clear the model variable
+  model = undefined;
+  	// let layerOfModel = "{"modelTopology": {"keras_version": "2.2.2", "backend": "tensorflow", "model_config": {"class_name": "Sequential", "config": [{"class_name": "Conv2D", "config": {"name": "conv2d_1", "trainable": true, "batch_input_shape": [null, 28, 28, 1], "dtype": "float32", "filters": 32, "kernel_size": [5, 5], "strides": [1, 1], "padding": "valid", "data_format": "channels_last", "dilation_rate": [1, 1], "activation": "relu", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}, {"class_name": "MaxPooling2D", "config": {"name": "max_pooling2d_1", "trainable": true, "pool_size": [2, 2], "padding": "valid", "strides": [2, 2], "data_format": "channels_last"}}, {"class_name": "Dropout", "config": {"name": "dropout_1", "trainable": true, "rate": 0.2, "noise_shape": null, "seed": null}}, {"class_name": "Flatten", "config": {"name": "flatten_1", "trainable": true, "data_format": "channels_last"}}, {"class_name": "Dense", "config": {"name": "dense_1", "trainable": true, "units": 128, "activation": "relu", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}, {"class_name": "Dense", "config": {"name": "dense_2", "trainable": true, "units": 10, "activation": "softmax", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}]}, "training_config": {"optimizer_config": {"class_name": "Adam", "config": {"lr": 0.0010000000474974513, "beta_1": 0.8999999761581421, "beta_2": 0.9990000128746033, "decay": 0.0, "epsilon": 1e-07, "amsgrad": false}}, "loss": "categorical_crossentropy", "metrics": ["accuracy"], "sample_weight_mode": null, "loss_weights": null}}, "weightsManifest": [{"paths": ["group1-shard1of1"], "weights": [{"name": "conv2d_1/kernel", "shape": [5, 5, 1, 32], "dtype": "float32"}, {"name": "conv2d_1/bias", "shape": [32], "dtype": "float32"}, {"name": "dense_1/kernel", "shape": [4608, 128], "dtype": "float32"}, {"name": "dense_1/bias", "shape": [128], "dtype": "float32"}, {"name": "dense_2/kernel", "shape": [128, 10], "dtype": "float32"}, {"name": "dense_2/bias", "shape": [10], "dtype": "float32"}]}]}"
+
+  // load the model using a HTTPS request (where you have stored your model files)
+  model = await tf.loadLayersModel("https://gogul09.github.io/models/" + modelName + "/model.json");
+  
+  console.log("model loaded..");
+}
+
+loadModel(modelName);
+
+//-----------------------------------------------
+// preprocess the canvas to be MLP friendly
+//-----------------------------------------------
+function preprocessCanvas(image, modelName) {
+
+	// if model is not available, send the tensor with expanded dimensions
+	if (modelName === undefined) {
+		alert("No model defined..")
+	} 
+
+	// if model is digitrecognizermlp, perform all the preprocessing
+	else if (modelName === "digitrecognizermlp") {
+		
+		// resize the input image to digitrecognizermlp's target size of (784, )
+		let tensor = tf.fromPixels(image)
+		    .resizeNearestNeighbor([28, 28])
+		    .mean(2)
+		    .toFloat()
+			.reshape([1 , 784]);
+		return tensor.div(255.0);
+	}
+
+	// if model is digitrecognizercnn, perform all the preprocessing
+	else if (modelName === "digitrecognizercnn") {
+		// resize the input image to digitrecognizermlp's target size of (1, 28, 28)
+		let tensor = tf.fromPixels(image)
+		    .resizeNearestNeighbor([28, 28])
+		    .mean(2)
+		    .expandDims(2)
+		    .expandDims()
+		    .toFloat();
+		console.log(tensor.shape);
+		return tensor.div(255.0);
+	}
+
+	// else throw an error
+	else {
+		alert("Unknown model name..")
+	}
+}
+
+//--------------------------------------------
+// predict function for digit recognizer mlp
+//--------------------------------------------
+async function predict() {
+
+	// get image data from canvas
+	var imageData = canvas.toDataURL();
+
+	// preprocess canvas
+	let tensor = preprocessCanvas(canvas, modelName);
+
+	// make predictions on the preprocessed image tensor
+	let predictions = await model.predict(tensor).data();
+
+	// get the model's prediction results
+	let results = Array.from(predictions)
+
+	// display the predictions in chart
+	displayChart(results)
+
+	console.log(results);
+}
+
+//------------------------------
+// Chart to display predictions
+//------------------------------
+var chart = "";
+var firstTime = 0;
+function loadChart(label, data, modelSelected) {
+	var ctx = document.getElementById('chart_box').getContext('2d');
+	chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'bar',
+
+	    // The data for our dataset
+	    data: {
+	        labels: label,
+	        datasets: [{
+	            label: modelSelected + " prediction",
+	            backgroundColor: '#f50057',
+	            borderColor: 'rgb(255, 99, 132)',
+	            data: data,
+	        }]
+	    },
+
+	    // Configuration options go here
+	    options: {}
+	});
+}
+
+//----------------------------
+// display chart with updated
+// drawing from canvas
+//----------------------------
+function displayChart(data) {
+	var select_model  = document.getElementById("select_model");
+  	var select_option = select_model.options[select_model.selectedIndex].value;
+	console.log("shubham"+ data.length);
+	label = ['0',  '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9',  'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z',  'a',  'b',  'd',  'e',  'f',  'g',  'h',  'n',  'q',  'r',  't']
+	if (firstTime == 0) {
+		loadChart(label, data, select_option);
+		firstTime = 1;
+	} else {
+		chart.destroy();
+		loadChart(label, data, select_option);
+	}
+	document.getElementById('chart_box').style.display = "block";
+}
